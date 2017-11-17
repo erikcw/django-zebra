f7a6fe5e48c5fe2ae8760c4f6c817be32b4f6637
Erik Wickstrom <erik@erikwickstrom.com>
2017-11-17 12:11:33 -0800

    Starting work on Python3 compatibility

--- zebra/forms.py
+++ zebra/forms.py
@@ -4,6 +4,7 @@ from django.utils.dates import MONTHS
 
 from zebra.conf import options
 from zebra.widgets import NoNameSelect, NoNameTextInput
+import six
 
 
 class MonospaceForm(forms.Form):
@@ -23,7 +24,7 @@ class StripePaymentForm(CardForm):
         self.fields['card_cvv'].label = "Card CVC"
         self.fields['card_cvv'].help_text = "Card Verification Code; see rear of card."
         months = [ (m[0], u'%02d - %s' % (m[0], unicode(m[1])))
-                    for m in sorted(MONTHS.iteritems()) ]
+                    for m in sorted(six.iteritems(MONTHS)) ]
         self.fields['card_expiry_month'].choices = months
 
     card_number = forms.CharField(required=False, max_length=20,
@@ -31,6 +32,6 @@ class StripePaymentForm(CardForm):
     card_cvv = forms.CharField(required=False, max_length=4,
         widget=NoNameTextInput())
     card_expiry_month = forms.ChoiceField(required=False, widget=NoNameSelect(),
-        choices=MONTHS.iteritems())
+        choices=six.iteritems(MONTHS))
     card_expiry_year = forms.ChoiceField(required=False, widget=NoNameSelect(),
         choices=options.ZEBRA_CARD_YEARS_CHOICES)
