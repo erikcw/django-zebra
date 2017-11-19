8b6b5948475f2507b1a0bd270116fb1a0dda1153
Erik Wickstrom <erik@erikwickstrom.com>
2017-11-18 16:19:10 -0800

    Removed unused imports

--- zebra/templatetags/zebra_tags.py
+++ zebra/templatetags/zebra_tags.py
@@ -1,8 +1,4 @@
-from django.core.urlresolvers import reverse
 from django import template
-from django.template.loader import render_to_string
-from django.utils.encoding import force_unicode
-from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 
 from zebra.conf import options
